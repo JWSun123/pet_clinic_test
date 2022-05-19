ddc08b2851bf94639a3a5efc72ddfb6709a4f1c1
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-05-18 22:06:47 -0400

    finish SpecialtyController, not tested yet.

--- src/main/java/com/pet/clinic/controller/SpecialtyController.java
+++ src/main/java/com/pet/clinic/controller/SpecialtyController.java
@@ -1,7 +1,67 @@
 package com.pet.clinic.controller;
 
+import com.pet.clinic.entity.Specialty;
+import com.pet.clinic.exception.RecordNotFoundException;
+import com.pet.clinic.service.SpecialtyService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class SpecialtyController {
+
+    private final SpecialtyService specialtyService;
+
+    public SpecialtyController(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
+    @GetMapping("/specialty")
+    public String getAllSpecialty(Model model) {
+        List<Specialty> specialtyList = specialtyService.getAllSpecialty();
+        model.addAttribute("specialties", specialtyList);
+        return "specialty-list";
+    }
+
+    @GetMapping("/addSpecialty")
+    public String addSpecialty(Model model) {
+        Specialty specialty = new Specialty();
+        model.addAttribute("specialty", specialty);
+        return "add-specialty";
+    }
+
+    @PostMapping("/saveSpecialty")
+    public String saveSpecialty(@Valid @ModelAttribute("specialty") Specialty newSpecialty, BindingResult result) throws RecordNotFoundException {
+        if (result.hasErrors()) {
+            return "add-specialty";
+        }
+        specialtyService.saveOrUpdateSpecialty(newSpecialty);
+        return "redirect:/specialty";
+    }
+
+    @GetMapping("/showSpecialtyForUpdate/{specialtyId}")
+    public String showSpecialtyForUpdate(@PathVariable(value = "specialtyId") Long id, Model model) throws RecordNotFoundException {
+        Specialty specialty = specialtyService.getSpecialtyById(id);
+        model.addAttribute("specialty", specialty);
+        return "update-specialty";
+    }
+
+    @PostMapping("/updateSpecialty")
+    public String updateSpecialty(@Valid @ModelAttribute("specialty") Specialty updateSpecialty, BindingResult result) throws RecordNotFoundException {
+        if (result.hasErrors()){
+            return "update-specialty";
+        }
+        specialtyService.saveOrUpdateSpecialty(updateSpecialty);
+        return "redirect:/specialty";
+    }
+
+    @GetMapping("/deleteSpecialty/{specialtyId}")
+    public String deleteSpecialty(@PathVariable(value = "specialtyId") Long id) throws RecordNotFoundException {
+        specialtyService.deleteSpecialtyById(id);
+        return "redirect: /specialty";
+    }
 }
--- src/main/java/com/pet/clinic/service/SpecialtyService.java
+++ src/main/java/com/pet/clinic/service/SpecialtyService.java
@@ -1,8 +1,45 @@
 package com.pet.clinic.service;
 
+import com.pet.clinic.entity.Specialty;
+import com.pet.clinic.exception.RecordNotFoundException;
+import com.pet.clinic.repository.SpecialtyRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
 
+    public SpecialtyService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    public List<Specialty> getAllSpecialty() {
+        return specialtyRepository.findAll();
+    }
+
+    public Specialty getSpecialtyById(Long id) throws RecordNotFoundException {
+        Optional<Specialty> specialty = specialtyRepository.findById(id);
+        if (specialty.isPresent()) {
+            return specialty.get();
+        }throw new RecordNotFoundException("Specialty Not Found");
+    }
+
+    public void saveOrUpdateSpecialty(Specialty newSpecialty) throws RecordNotFoundException {
+        if (newSpecialty.getId() == null) {
+            specialtyRepository.save(newSpecialty);
+        }
+        else{
+            Specialty specialty = getSpecialtyById(newSpecialty.getId());
+            specialty.setSpecialtyName(newSpecialty.getSpecialtyName());
+            specialtyRepository.save(specialty);
+        }
+    }
+
+    public void deleteSpecialtyById(Long id) throws RecordNotFoundException {
+        getSpecialtyById(id);
+        specialtyRepository.deleteById(id);
+    }
 }
