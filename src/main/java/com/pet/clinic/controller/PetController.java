ea9853126a494d4eacdd968cf0af75896a226be5
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-05-24 16:50:09 -0400

    petController

--- src/main/java/com/pet/clinic/controller/PetController.java
+++ src/main/java/com/pet/clinic/controller/PetController.java
@@ -1,8 +1,18 @@
 package com.pet.clinic.controller;
 
+import com.pet.clinic.entity.Pet;
+import com.pet.clinic.exception.RecordNotFoundException;
 import com.pet.clinic.service.PetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class PetController {
@@ -12,4 +22,51 @@ public class PetController {
         this.petService = petService;
     }
 
+    @GetMapping("/pet")
+    public String getAllPets(Model model){
+        List<Pet> petList = petService.getAllPets();
+        model.addAttribute("pets", petList);
+        return "pet-list";
+    }
+
+    @GetMapping("/addPet")
+    public String addPet(Model model){
+        Pet pet = new Pet();
+        model.addAttribute("pet", pet);
+        return "add-pet";
+    }
+
+    @PostMapping("/savePet")
+    public String savePet(@Valid @ModelAttribute("pet") Pet newPet, BindingResult result) throws RecordNotFoundException {
+        if (result.hasErrors()) {
+            return "/add-pet";
+        }
+        petService.saveOrUpdatePet(newPet);
+        return "redirect:/pet";
+    }
+
+    // when clicking on the modify button beside a pet. show a form for update pet info(not include specialty)
+    // a separate button for view/modify pet's specialty.
+    @GetMapping("/showPetInfoForUpdate/{petId}")
+    public String showPetInfoForUpdate(@PathVariable(value = "petId") Long petId, Model model) throws RecordNotFoundException {
+        Pet pet = petService.getPetById(petId);
+        model.addAttribute("pet", pet);
+        return "update-pet-info";
+    }
+
+    //updating info, won't open any new page.
+    @PostMapping("/updatePetInfo")
+    public String updatePetInfo(@Valid @ModelAttribute("pet") Pet updatePet, BindingResult result) throws RecordNotFoundException {
+        if (result.hasErrors()) {
+            return "/update-pet-info";
+        }
+        petService.saveOrUpdatePet(updatePet);
+        return "redirect:/pet";
+    }
+
+    @GetMapping("/deletePet/{petId}")
+    public String deletePet(@PathVariable(value = "petId") Long petId) {
+        petService.deletePet(petId);
+        return "redirect:../pet";
+    }
 }
--- src/main/java/com/pet/clinic/service/PetService.java
+++ src/main/java/com/pet/clinic/service/PetService.java
@@ -3,9 +3,12 @@ package com.pet.clinic.service;
 import com.pet.clinic.entity.Pet;
 import com.pet.clinic.exception.RecordNotFoundException;
 import com.pet.clinic.repository.PetRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PetService {
@@ -25,4 +28,33 @@ public class PetService {
             throw new RecordNotFoundException("Pet Not Found");
         }
     }
+
+    public List<Pet> getAllPets() {
+        return petRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
+
+    public Pet getPetById(Long id) throws RecordNotFoundException{
+        Optional<Pet> pet = petRepository.findById(id);
+        if (pet.isPresent()){
+            return pet.get();
+        }
+        throw new RecordNotFoundException("Pet Not Found");
+    }
+
+    public void saveOrUpdatePet(Pet newPet) throws RecordNotFoundException{
+        if (newPet.getId() == null){
+            petRepository.save(newPet);
+        }else{
+            Pet petFromDb = getPetById(newPet.getId());
+            petFromDb.setPetName(newPet.getPetName());
+            petFromDb.setDob(newPet.getDob());
+            petFromDb.setPetType(newPet.getPetType());
+            petRepository.save(petFromDb);
+        }
+    }
+
+    public void deletePet(Long petId){
+        petRepository.deleteById(petId);
+    }
+
 }
